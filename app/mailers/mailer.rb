550ff4c0ce10977e4556c7b73b975b33657d9079
Drillan767 <jd.levarato@gmail.com>
2016-11-24 15:16:57 +0100

    Contact terminé mais non paramétré

---
+++ app/mailers/mailer.rb
@@ -0,0 +1,17 @@
+class Mailer < ActionMailer::Base
+
+  layout 'mailer'
+
+  def contact_form(contact)
+
+    @contact = contact
+    @to = 'kippondream@gmail.com'
+    @from = 'kippondream.com'
+
+    mail(to: @to, from: @from, subject: 'Nouveau contact depuis le site !') do |f|
+      f.html
+    end
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
   resources :scans
   resources :magazines
   resources :pages
   resources :mangas
+
+  # Partie contact
+  get '/contact', to: 'contacts#new', as: :new_contact
+  post '/contact', to: 'contacts#create', as: :contacts
+
   root to: 'home#index'
 end
