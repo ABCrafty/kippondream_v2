a43b92eb5efd57d0ae6be9ad4e31b632db0bd8fa
Alexandre Black <alexandreblack@hotmail.fr>
2016-12-12 19:11:15 +0100

    optimisation d'affichage des news, intégration du carousel dans le header -en cours-

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -6,5 +6,15 @@ $(document).ready(function () {
         });
     }
 
-    $('.owl-carousel').owlCarousel();
+    $('div#header-slider').owlCarousel({
+        nav : true, // Show next and prev buttons
+        navSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true,
+        items: 1,
+        loop: true
+    });
+
+
+
 }); // fermeture jquery
