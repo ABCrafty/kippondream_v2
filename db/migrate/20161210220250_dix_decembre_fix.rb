3f071b82320b1fdf860d835249f2e4cdfe8bc5a4
drillan767 <jd.levarato@gmail.com>
2016-12-12 10:11:33 +0100

    commit de migration

--- db/migrate/20161210220250_dix_decembre_fix.rb
+++ db/migrate/20161210220250_dix_decembre_fix.rb
@@ -3,7 +3,5 @@ class DixDecembreFix < ActiveRecord::Migration[5.0]
     add_column :blog, :slug, :string
     add_column :blog, :user_id, :integer
     add_column :magazines, :slug, :string
-    add_column :pages, :magazine_id, :integer
-    add_column :pages, :page_number, :integer
   end
 end
