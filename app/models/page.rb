092b080955aa0f4cc22132a1159a437cac68b295
Drillan767 <jd.levarato@gmail.com>
2016-12-14 11:49:19 +0100

    Ajout d'un lien permettant la navigation entre les pages des magazines et des mangas

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,4 +4,12 @@ class Page < ApplicationRecord
   validates_presence_of :magazine
   mount_uploader :image, PageUploader
 
+  def previous
+    self.class.where('page_number < ?', page_number).limit(1).first
+  end
+
+  def next
+    self.class.where('page_number > ?', page_number).limit(1).last
+  end
+
 end
--- app/models/peji.rb
+++ app/models/peji.rb
@@ -4,4 +4,12 @@ class Peji < ApplicationRecord
   validates_presence_of :manga
   mount_uploader :image, ScansUploader
 
+  def previous
+    self.class.where('scan_number < ?', scan_number).limit(1).first
+  end
+
+  def next
+    self.class.where('scan_number > ?', scan_number).limit(1).last
+  end
+
 end
