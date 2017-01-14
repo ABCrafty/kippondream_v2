70ca4eca8b17bfedfd097f6090094303e8a392a2
Alexandre Black <alexandreblack@hotmail.fr>
2017-01-14 19:45:16 +0100

    intégration page magazines (à finaliser) et intégration page séries (en cours)

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -16,6 +16,18 @@ $(document).ready(function () {
         navigationText : ['<i class="fa fa-arrow-left" aria-hidden="true"></i>', '<i class="fa fa-arrow-right" aria-hidden="true"></i>']
     });
 
+    $('div#mag-carousel').owlCarousel({
+        navigation: true,
+        pagination: true,
+        paginationNumbers: true,
+        slideSpeed : 1000,
+        paginationSpeed : 1000,
+        singleItem: true,
+        autoPlay: 6000,
+        stopOnHover: true,
+        navigationText : ['<i class="fa fa-arrow-up" aria-hidden="true"></i>', '<i class="fa fa-arrow-down" aria-hidden="true"></i>']
+    })
+
 
 
 }); // fermeture jquery
