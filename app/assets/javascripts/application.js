d083f82132d2843a7c4319e9c42fb6cad86ec388
Drillan767 <jd.levarato@gmail.com>
2017-02-27 16:14:55 +0100

    Ajout de TinyMCE, fonctionne avec le moindre textarea tavu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,8 @@
 //= require jquery_ujs
 // Rajouter des fichiers à partir d'ici
 //= require owl-carousel/owl-carousel/owl.carousel.js
-// require tinymce/tinymce.js
+//= require tinymce/tinymce.min.js
+//= require tinymce/themes/modern/theme.js
 //= require script
 //= require turbolinks
 //= require_tree .
--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,80 +1,82 @@
 $(document).ready(function () {
 
+  tinymce.init({
+    selector: 'textarea'
+  }
+
+  $('div#header-slider').owlCarousel({
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 800,
+      paginationSpeed : 1000,
+      singleItem:true,
+      autoPlay: 5000,
+      stopOnHover: true,
+      navigationText : ['<i class="fa fa-arrow-left" aria-hidden="true"></i>', '<i class="fa fa-arrow-right" aria-hidden="true"></i>']
+  });
+
+  $('div#mag-carousel').owlCarousel({
+      navigation: true,
+      pagination: true,
+      paginationNumbers: true,
+      slideSpeed : 1000,
+      paginationSpeed : 1000,
+      singleItem: true,
+      autoPlay: 6000,
+      stopOnHover: true,
+      navigationText : ['<i class="fa fa-arrow-up" aria-hidden="true"></i>', '<i class="fa fa-arrow-down" aria-hidden="true"></i>']
+  });
 
 
-    $('div#header-slider').owlCarousel({
-        navigation : true, // Show next and prev buttons
-        slideSpeed : 800,
-        paginationSpeed : 1000,
-        singleItem:true,
-        autoPlay: 5000,
-        stopOnHover: true,
-        navigationText : ['<i class="fa fa-arrow-left" aria-hidden="true"></i>', '<i class="fa fa-arrow-right" aria-hidden="true"></i>']
-    });
+  var $animationElements = $('.kd-animate');
+  var $window = $(window);
 
-    $('div#mag-carousel').owlCarousel({
-        navigation: true,
-        pagination: true,
-        paginationNumbers: true,
-        slideSpeed : 1000,
-        paginationSpeed : 1000,
-        singleItem: true,
-        autoPlay: 6000,
-        stopOnHover: true,
-        navigationText : ['<i class="fa fa-arrow-up" aria-hidden="true"></i>', '<i class="fa fa-arrow-down" aria-hidden="true"></i>']
-    });
+  var scroll = {
+      checkView : function () {
+          var windowHeight = $window.height();
+          var windowTopPosition = $window.scrollTop();
+          var windowBottomPosition = (windowTopPosition + windowHeight);
 
+          $.each($animationElements, function() {
+              var $element = $(this);
+              var elementHeight = $element.outerHeight();
+              var elementTopPosition = $element.offset().top;
+              var elementBottomPosition = (elementTopPosition + elementHeight);
 
-    var $animationElements = $('.kd-animate');
-    var $window = $(window);
+              //check to see if this current container is within viewport
+              if ((elementBottomPosition >= windowTopPosition) &&
+                  (elementTopPosition <= windowBottomPosition)) {
+                  $element.addClass('in-view');
+              } else {
 
-    var scroll = {
-        checkView : function () {
-            var windowHeight = $window.height();
-            var windowTopPosition = $window.scrollTop();
-            var windowBottomPosition = (windowTopPosition + windowHeight);
-
-            $.each($animationElements, function() {
-                var $element = $(this);
-                var elementHeight = $element.outerHeight();
-                var elementTopPosition = $element.offset().top;
-                var elementBottomPosition = (elementTopPosition + elementHeight);
-
-                //check to see if this current container is within viewport
-                if ((elementBottomPosition >= windowTopPosition) &&
-                    (elementTopPosition <= windowBottomPosition)) {
-                    $element.addClass('in-view');
-                } else {
-
-                }
-            });
-        } // attribut checkView
-    }; // objet scroll
+              }
+          });
+      } // attribut checkView
+  }; // objet scroll
 
 //bind click events
-    var $cell = $('.membre');
+  var $cell = $('.membre');
 
-    $cell.find('.image-basic').click(function() {
-        var $thisCell = $(this).closest('.membre');
+  $cell.find('.image-basic').click(function() {
+      var $thisCell = $(this).closest('.membre');
 
-        if ($thisCell.hasClass('is-collapsed')) {
-            $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed');
-            $thisCell.removeClass('is-collapsed').addClass('is-expanded');
-        } else {
-            $thisCell.removeClass('is-expanded').addClass('is-collapsed');
-        }
-    });
+      if ($thisCell.hasClass('is-collapsed')) {
+          $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed');
+          $thisCell.removeClass('is-collapsed').addClass('is-expanded');
+      } else {
+          $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+      }
+  });
 
-    $cell.find('.expand-close').click(function(){
+  $cell.find('.expand-close').click(function(){
 
-        var $thisCell = $(this).closest('.membre');
+      var $thisCell = $(this).closest('.membre');
 
-        $thisCell.removeClass('is-expanded').addClass('is-collapsed');
-    });
+      $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+  });
 
-    // exécution des fonctions
+  // exécution des fonctions
 
-    $(window).on('scroll resize', scroll.checkView);
-    $(window).trigger('scroll');
+  $(window).on('scroll resize', scroll.checkView);
+  $(window).trigger('scroll');
 
 }); // fermeture jquery
